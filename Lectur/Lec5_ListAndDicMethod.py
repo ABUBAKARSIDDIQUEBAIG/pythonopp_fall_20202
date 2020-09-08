52ec6813da5b82f039f54cb652bf6cf90d252d6b
ABUBAKAR SIDDIQUEBAIG <abubakarsiddiquebaig1900@gmail.com>
2020-09-07 21:47:57 -0700

    few changes

---
+++ Lectur/Lec5_ListAndDicMethod.py
@@ -0,0 +1,19 @@
+list1 =  [12,23,34,45,54,43,21]
+list2 = list1.copy()
+
+#_student_marks =   [ ]
+
+
+
+
+list1.append(100)
+
+print(list1)
+print(list2)
+
+list1.insert(2,444)
+list1.sort()
+print(list1)
+
+print(len(list1))
+print(len(list2))
---
+++ lab/lab1/introduction_of_python.py
@@ -0,0 +1,10 @@
+#What is python
+#ANS:
+#Python is a popular programming language.
+# It was created by Guido van Rossum, and released in 1991.
+#It is used for:
+#web development (server-side),
+#software development,
+#mathematics,
+#system scripting
+print('hello world')
