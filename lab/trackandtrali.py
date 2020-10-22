a807e14e5f27f7d87c2e6362d6b53fd4b299976b
ABUBAKARSIDDIQUEBAIG <70310148+ABUBAKARSIDDIQUEBAIG@users.noreply.github.com>
2020-10-21 22:41:59 -0700

    Create trackandtrali.py

---
+++ lab/trackandtrali.py
@@ -0,0 +1,54 @@
+#operator overloding
+class vehical:
+  no_of_wheel=0
+  no_of_lights=""
+ 
+  def introduce_whecal(self ):
+    print("I ma a simple whecal with: " +self.no_of_wheel+"Waheles")
+    print("I have "+self.no_of_lights+"Lights")
+  def__add_(self,other)
+  temp1=self.no_of_lights+other.no_of_lights
+  temp2=self.no_of_wheel+other.no_of_wheel
+  return [temp1,temp2]
+trali=vehical()
+tractor=vehical()
+tractor.no_of_wheel=4
+tractor.no_of_lights=2
+trali.no_of_wheel=3
+trali.no_of_lights=5
+
+tractortrali=tractor+trali
+print("no of whecal of tractortrali"+ tractortrali  [0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
